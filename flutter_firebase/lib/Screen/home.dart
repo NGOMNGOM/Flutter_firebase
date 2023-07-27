6a5874cc61a134b3808b2c5ec853b877da3a6314
NGOMNGOM <takongbaza@gmail.com>
2023-07-27 18:25:47 +0700

    ์Navigation

--- flutter_firebase/lib/Screen/home.dart
+++ flutter_firebase/lib/Screen/home.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:flutter_firebase/Screen/login.dart";
+import "package:flutter_firebase/Screen/register.dart";
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -29,14 +31,24 @@ class _HomeState extends State<Home> {
                   child: ElevatedButton.icon(
                     icon: Icon(Icons.app_registration),
                     label: Text("Registration"),
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return Register();
+                      }));
+                    },
                   )),
               SizedBox(
                   width: double.infinity,
                   child: ElevatedButton.icon(
                     icon: Icon(Icons.login),
                     label: Text("Login"),
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return Login();
+                      }));
+                    },
                   )),
             ],
           ),
